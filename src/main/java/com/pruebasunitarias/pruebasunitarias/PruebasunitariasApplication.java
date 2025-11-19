76180c40f0fe26e0a735305aaa94ef3c9f678415
nico4all <enicolassancheztobar@gmail.com>
2025-11-19 17:14:44 -0500

    first commit

---
+++ src/main/java/com/pruebasunitarias/pruebasunitarias/PruebasunitariasApplication.java
@@ -0,0 +1,13 @@
+package com.pruebasunitarias.pruebasunitarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebasunitariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebasunitariasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebasunitarias/pruebasunitarias/PruebasunitariasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebasunitarias.pruebasunitarias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebasunitariasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
